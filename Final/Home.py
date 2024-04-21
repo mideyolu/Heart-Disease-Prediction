a5f0a68f936e36a3fcbbdb0bf014bff5695372ec
mideyolu <olumideoluwuyi91.com>
2024-04-21 22:34:34 +0100

    update

---
+++ Final/Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Home Page",
+    page_icon="👋🏾",
+)
+
+st.write(
+    "# Welcome to the Heart Disease Prediction Web App "
+)
+
+st.sidebar.success("Select a page above.")
+
+st.markdown('Developed by Oluwuyi Olumide!')
